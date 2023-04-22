3c47cbe1b1b866c0e170906f3fcba07b58003e36
a-rasines <alberto.rasines@opendeusto.es>
2023-04-22 18:23:43 +0200

    Fixed duration format

--- src/lavaplayer/PlayerManager.java
+++ src/lavaplayer/PlayerManager.java
@@ -225,7 +225,7 @@ public class PlayerManager {
             }
             private String formatTime(long timeInMillis) {
 		        final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
-		        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1);
+		        final long minutes = timeInMillis % TimeUnit.HOURS.toMillis(1) / TimeUnit.MINUTES.toMillis(1);
 		        final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
 
 		        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
